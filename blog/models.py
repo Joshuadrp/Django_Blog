50fbad9843298c54bc903629a4e0b5cca8a37652
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-08-19 15:32:14 +0000

    Add Comment model to models.py, migration completed.

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,14 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="comments", null=True, blank=True)
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now=True)
+    
+
